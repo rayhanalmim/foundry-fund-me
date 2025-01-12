ee384e6999b4964b1ed9cc56a2ca08e66261b19b
rayhanalmim <rayhanalmim1@gmail.com>
2025-01-12 18:01:57 +0600

    add multiple rpc to test the contract

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -6,11 +6,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
     FundMe fundMe;
+    address USER = makeAddr("USER");
+    uint256 public constant SEND_VALUE = 0.1 ether;
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
+
     function setUp() external {
-        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
-        
+        vm.deal(USER, STARTING_USER_BALANCE);
     }
     function testMinimumDollersFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
@@ -22,5 +26,17 @@ contract FundMeTest is Test{
         uint256 version = fundMe.getVersion();
         console.log("Version", version);
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundSuccessWithEnoughETH() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
